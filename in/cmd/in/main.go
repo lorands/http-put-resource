dce4f720f169605cafc5fa4fde0978ce53adf794
lorands <lorand.somogyi@plensys.com>
2018-11-15 22:38:55 +0100

    test logs

--- in/cmd/in/main.go
+++ in/cmd/in/main.go
@@ -12,24 +12,33 @@ import (
 
 func main() {
 	var request in.Request
+
+	fmt.Println("Request:", request)
+
 	if err := json.NewDecoder(os.Stdin).Decode(&request); err != nil {
 		fatal("reading request from stdin", err)
 	}
+
+	fmt.Println("Request:", request)
 
 	timestamp := request.Version.Timestamp
 	if timestamp.IsZero() {
 		timestamp = time.Now()
 	}
 
+	fmt.Println("TS:", timestamp)
+
 	response := in.Response{
 		Version: resource.Version{
 			Timestamp: timestamp,
 		},
 	}
+	fmt.Println("Response:", response)
 
 	if err := json.NewEncoder(os.Stdout).Encode(response); err != nil {
 		fatal("writing response", err)
 	}
+	fmt.Println("Done.")
 }
 
 func fatal(message string, err error) {
